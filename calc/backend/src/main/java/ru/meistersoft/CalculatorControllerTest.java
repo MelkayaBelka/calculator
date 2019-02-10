7f012a1fd6da89b5ebc163347978de8f12a02099
nsmirnova <>
2019-02-10 18:42:55 +0300

    GymCalc_test

---
+++ calc/backend/src/main/java/ru/meistersoft/CalculatorControllerTest.java
@@ -0,0 +1,34 @@
+package ru.meistersoft;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCalculator() {
+    }
+
+    @Test
+    void getCalculatorState() {
+    }
+
+    @Test
+    void sendCommand() {
+    }
+
+    @Test
+    void deleteCalculator() {
+    }
+}
---
+++ calc/backend/src/main/java/ru/meistersoft/CalculatorStackTest.java
@@ -0,0 +1,24 @@
+package ru.meistersoft;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorStackTest {
+
+    @Test
+    void pushValue() {
+    }
+
+    @Test
+    void pushOperator() {
+    }
+
+    @Test
+    void getResult() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
